89f680236d0e35dee497ceba35894aed50cabf55
Josue Trejo <josuedu96@gmail.com>
2022-09-28 19:39:39 -0500

    Make button generate passwords 15 characters long

--- javascript.js
+++ javascript.js
@@ -1,2 +1,14 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+const genPassBtn = document.getElementById("gen-pass-btn");
+const passOne = document.getElementById("pass-btn-one");
+const passTwo = document.getElementById("pass-btn-two");
+
+function generatePasswords() {
+    for (let i = 0; i < 15; i++) {
+        let firstPass = Math.floor( Math.random() * characters.length );
+        let secondPass = Math.floor( Math.random() * characters.length );
+        passOne.textContent += characters[firstPass];
+        passTwo.textContent += characters[secondPass];
+    }
+}
